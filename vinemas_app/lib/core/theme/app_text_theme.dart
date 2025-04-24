c7d3e1b4366a7d3e942e5515eaca9038875daffd
Pungz <phungvandung030623@mail.com>
2025-04-24 15:48:11 +0700

    Remove Firebase config files from version control

---
+++ vinemas_app/lib/core/theme/app_text_theme.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:vinemas_app/core/config/app_color.dart';
+
+class AppTextTheme {
+  static const TextTheme textTheme = TextTheme(
+    displayLarge: TextStyle(
+      color: AppColor.primaryTextColor,
+      fontSize: 57,
+      fontWeight: FontWeight.bold,
+    ),
+    displayMedium: TextStyle(
+      color: AppColor.primaryTextColor,
+      fontSize: 45,
+      fontWeight: FontWeight.bold,
+    ),
+    displaySmall: TextStyle(
+      color: AppColor.primaryTextColor,
+      fontSize: 36,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineLarge: TextStyle(
+      color: AppColor.primaryTextColor,
+      fontSize: 32,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineMedium: TextStyle(
+      color: AppColor.primaryTextColor,
+      fontSize: 28,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineSmall: TextStyle(
+      color: AppColor.primaryTextColor,
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+    ),
+    titleLarge: TextStyle(
+      color: AppColor.primaryTextColor,
+      fontSize: 22,
+      fontWeight: FontWeight.bold,
+    ),
+    titleMedium: TextStyle(
+      color: AppColor.primaryTextColor,
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+    ),
+    titleSmall: TextStyle(
+      color: AppColor.primaryTextColor,
+      fontSize: 14,
+      fontWeight: FontWeight.bold,
+    ),
+    labelLarge: TextStyle(
+      color: AppColor.primaryTextColor,
+      fontSize: 14,
+      fontWeight: FontWeight.bold,
+    ),
+    labelMedium: TextStyle(
+      color: AppColor.primaryTextColor,
+      fontSize: 12,
+      fontWeight: FontWeight.bold,
+    ),
+    labelSmall: TextStyle(
+      color: AppColor.primaryTextColor,
+      fontSize: 11,
+      fontWeight: FontWeight.bold,
+    ),
+    bodyLarge: TextStyle(
+      color: AppColor.primaryTextColor,
+      fontSize: 16,
+      fontWeight: FontWeight.normal,
+    ),
+    bodyMedium: TextStyle(
+      color: AppColor.primaryTextColor,
+      fontSize: 14,
+      fontWeight: FontWeight.normal,
+    ),
+    bodySmall: TextStyle(
+      color: AppColor.primaryTextColor,
+      fontSize: 12,
+      fontWeight: FontWeight.normal,
+    ),
+  );
+}
